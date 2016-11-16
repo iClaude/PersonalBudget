ea9e614bfc08361ef160b4ee3169d0f785e555ec
iClaude <agostini.claudio1@gmail.com>
2016-11-16 16:18:56 +0100

    constraing layout on budget details implemented: bugs to fix

--- app/src/main/java/com/flingsoftware/personalbudget/app/budgets/BudgetDetailsData.java
+++ app/src/main/java/com/flingsoftware/personalbudget/app/budgets/BudgetDetailsData.java
@@ -56,7 +56,7 @@ public class BudgetDetailsData extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.budget_details_data, container, false);
+        View view = inflater.inflate(R.layout.budget_details_data2, container, false);
 
         id = getArguments().getLong(KEY_ID);
         // Get references of widgets.
